312bc06f273af73e7cebfe817fb3a01979d4967f
Casey <caseyclinga@gmail.com>
2020-01-15 13:20:15 -0800

    Added functionality to display date

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -6,8 +6,23 @@ const api = {
 }
 
 function App() {
+
+  const dateBuilder = d => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", 
+    "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
+     "Saturday"];
+
+     let day = days[d.getDay()];
+     let date = d.getDate();
+     let month = months[d.getMonth()];
+     let year = d.getFullYear();
+
+     return `${day} ${date} ${month} ${year}`;
+  }
+
   return (
-    <div className="App">
+    <div className="app">
       <main>
         <div className="search-box">
           <input
@@ -15,6 +30,10 @@ function App() {
             className="search-bar"
             placeholder="Search..."
           />
+        </div>
+        <div className="location-box">
+        <div className="location" style={{color: "grey"}}>New York City, US</div>
+        <div className="date" style={{color: "grey"}}>{dateBuilder(new Date())}</div>
         </div>
       </main>
     </div>
