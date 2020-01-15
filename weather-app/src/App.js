17b767fae191edb04c86c70a5f917e4d35258b02
Casey <caseyclinga@gmail.com>
2020-01-15 13:58:23 -0800

    Set up fetch API to query data base and return results

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,39 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const api = {
   key: "605551f3c844dd9d25b4f909923c558a",
-  base: "https://http://api.openweathermap.org/data/2.5/"
+  base: "https://api.openweathermap.org/data/2.5/"
 }
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = e => {
+    if (e.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        })
+    }
+  }
 
   const dateBuilder = d => {
-    let months = ["January", "February", "March", "April", "May", "June", "July", 
-    "August", "September", "October", "November", "December"];
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+      "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
-     "Saturday"];
+      "Saturday"];
 
-     let day = days[d.getDay()];
-     let date = d.getDate();
-     let month = months[d.getMonth()];
-     let year = d.getFullYear();
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
 
-     return `${day} ${date} ${month} ${year}`;
+    return `${day} ${date} ${month} ${year}`;
   }
 
   return (
-    <div className="app">
+    <div className="app" style={{ color: "#FFF" }}>
       <main>
         <div className="search-box">
           <input
             type="text"
             className="search-bar"
             placeholder="Search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
           />
         </div>
         <div className="location-box">
-        <div className="location" style={{color: "grey"}}>New York City, US</div>
-        <div className="date" style={{color: "grey"}}>{dateBuilder(new Date())}</div>
+          <div className="location">New York City, US</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">
+            50° F
+          </div>
+          <div className="weather">Snowy</div>
         </div>
       </main>
     </div>
